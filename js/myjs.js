37b29d7ab241317fa95f23f0de9755d98afb12f5
huangzhengyu <aaron.yu.huang@gmail.com>
2017-05-04 11:16:56 +0800

    init project

---
+++ js/myjs.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+			$(".list_cop").hide();
+
+			$(".other_word01").hover(function(){
+				$(".list_cop").show();
+			},function(){
+				$(".list_cop").hide();
+			});
+
+			$(".list_cop").hover(function(){
+				$(".list_cop").show();
+			},function(){
+				$(".list_cop").hide();
+			});
+
+
+
+			$(".list_pro").hide();
+
+			$(".other_word02").hover(function(){
+				$(".list_pro").show();
+			},function(){
+				$(".list_pro").hide();
+			});
+
+			$(".list_pro").hover(function(){
+				$(".list_pro").show();
+			},function(){
+				$(".list_pro").hide();
+			});
+});
